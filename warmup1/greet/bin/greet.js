39aa4ff2553156208d019caf8d00b8b58f7e2057
DoubleShift <fjjwlzd@163.com>
2014-04-29 21:38:02 +0800

    rm old

--- warmup1/greet/bin/greet.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-var greet = require('../index.js');
-var parseArgs = require('minimist');
-var argv = parseArgs(process.argv,opts = {}); 
-console.log(greet(process.argv[2],argv.drunk));
