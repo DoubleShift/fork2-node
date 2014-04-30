086631ddef9c8f7981e1836cc2392026e75440a3
Double Shift <fjjwlzd@163.com>
2014-05-01 00:01:56 +0800

    CoffeScript && Makefile

--- warmup/greet/bin/greet.js
+++ warmup/greet/bin/greet.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-var greet = require('../index.js');
-var parseArgs = require('minimist');
-var argv = parseArgs(process.argv,opts = {}); 
-console.log(greet(process.argv[2],argv.drunk));
+//var greet = require('../lib/index.js');
+//var parseArgs = require('minimist');
+//var argv = parseArgs(process.argv,opts = {}); 
+//console.log(greet(process.argv[2],argv.drunk));
+
+
+var command = require("../lib/command");
+command();
